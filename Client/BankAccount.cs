afae2e54d3fe39759f97d83f384fa5eb60b7b1d2
ME <ME@DESKTOP-1TLGLRF>
2023-02-27 16:31:58 +0330

    Add project files.

---
+++ Client/BankAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace Client
+{
+    public class BankAccount
+    {
+        public int AccountNumbers { get; set; }
+
+        public int Pin { get; set; }
+    }
+}
---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(BankAccount bankAccount)
+        {
+            return Content($"Account {bankAccount.AccountNumbers} has been updated with new  pin -{bankAccount.Pin}");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
